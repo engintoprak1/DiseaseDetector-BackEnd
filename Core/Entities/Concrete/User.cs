791d6351ea139cf9a9f9989418fc5cdbbc3011c2
engintoprak1 <engin.toprak41@hotmail.com>
2022-05-30 17:57:39 +0300

    Updated user entity in core

--- Core/Entities/Concrete/User.cs
+++ Core/Entities/Concrete/User.cs
@@ -5,18 +5,14 @@ using System.Text;
 
 namespace Core.Entities.Concrete
 {
-    public class User:IEntity
+    public class User : IEntity
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string MobilePhone { get; set; }
-        public string Address { get; set; }
-        public byte[] PasswordSalt { get; set; }
+        public string IdentificationNumber { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Gsm { get; set; }
         public byte[] PasswordHash { get; set; }
-        public bool Status { get; set; }
-
-
+        public byte[] PasswordSalt { get; set; }
     }
 }
