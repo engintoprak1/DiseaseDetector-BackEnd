9282f29b643dccdd82b91a3c7c9b8522d9fb6045
engintoprak1 <engin.toprak41@hotmail.com>
2022-05-30 17:55:44 +0300

    The relationship between the entities and the database was established with the entity framework.

---
+++ DataAccess/Concrete/EntityFramework/DiseaseDetectionContext.cs
@@ -0,0 +1,34 @@
+﻿using Core.Entities.Concrete;
+using Core.Utilities.IoC;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class DiseaseDetectionContext : DbContext
+    {
+        private IConfiguration _configuration;
+        public DiseaseDetectionContext()
+        {
+            _configuration = ServiceTool.ServiceProvider.GetService<IConfiguration>();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("MSSQL_Server"));
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Question> Questions { get; set; }
+
+        public DbSet<Policlinic> Policlinics { get; set; }
+
+        public DbSet<Hospital> Hospitals { get; set; }
+
+        public DbSet<BodyPart> BodyParts { get; set; }
+
+        public DbSet<Answer> Answers { get; set; }
+    }
+}
